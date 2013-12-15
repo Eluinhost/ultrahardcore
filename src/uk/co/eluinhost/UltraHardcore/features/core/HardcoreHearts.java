5b4d9b973a5222cce7ac6e6fb426c339d9ef0b0b
Graham <graham_howden1@yahoo.co.uk>
2013-12-15 11:17:25 +0000

    Fixed deprecated way of making hardcore hearts packet editing work

--- src/uk/co/eluinhost/UltraHardcore/features/core/HardcoreHearts.java
+++ src/uk/co/eluinhost/UltraHardcore/features/core/HardcoreHearts.java
@@ -1,13 +1,11 @@
 package uk.co.eluinhost.UltraHardcore.features.core;
 
-import com.comphenix.protocol.Packets;
+import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ConnectionSide;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.injector.GamePhase;
 import uk.co.eluinhost.UltraHardcore.UltraHardcore;
 import uk.co.eluinhost.UltraHardcore.features.UHCFeature;
 
@@ -25,14 +23,14 @@ public class HardcoreHearts extends UHCFeature{
 	@Override
 	public void enableFeature() {
 		if(pa == null){
-			pa = new PacketAdapter(UltraHardcore.getInstance(), ConnectionSide.SERVER_SIDE, 
-	                  ListenerPriority.NORMAL, GamePhase.LOGIN, Packets.Server.LOGIN){
+			//pa = new PacketAdapter(UltraHardcore.getInstance(), ConnectionSide.SERVER_SIDE,
+	          //        ListenerPriority.NORMAL, GamePhase.LOGIN, Packets.Server.LOGIN){
+            pa = new PacketAdapter(UltraHardcore.getInstance(),ListenerPriority.NORMAL,
+                    PacketType.Play.Server.LOGIN) {
 	        	  @Override
 	              public void onPacketSending(PacketEvent event) {
-	        		  switch(event.getPacketID()){
-	        		  case Packets.Server.LOGIN:
+	        		  if(event.getPacketType() == PacketType.Play.Server.LOGIN){
 	        			  event.getPacket().getBooleans().write(0, true);
-	        			 //event.getPacket().getBytes().write(2, (byte) 0);
 	        		  }
 	        	  }
 	          };
