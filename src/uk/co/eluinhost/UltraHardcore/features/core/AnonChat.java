88f0281d77d6c78641237e4693ac0baa5613fe26
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-21 09:58:13 +0000

    documentation don't split the message and then recompile it >.>

--- src/uk/co/eluinhost/UltraHardcore/features/core/AnonChat.java
+++ src/uk/co/eluinhost/UltraHardcore/features/core/AnonChat.java
@@ -11,21 +11,35 @@ import uk.co.eluinhost.UltraHardcore.features.UHCFeature;
 
 public class AnonChat extends UHCFeature {
 
+    /**
+     * The format before every message sent
+     */
     private static final String PREFIX = ChatColor.RESET + "<" + ChatColor.MAGIC + "SECRET" + ChatColor.RESET + ">";
 
+    /**
+     * Construct an anonchat feature
+     * @param enabled enabled by default or not
+     */
     public AnonChat(boolean enabled) {
         super("AnonChat", enabled);
         setDescription("Allows players to chat without revealing their name");
     }
 
+    /**
+     * On player chat events
+     * @param apce AsyncPlayerChatEvent
+     */
     @EventHandler
     public void onAsyncChatEvent(AsyncPlayerChatEvent apce) {
+        //only if we are enabled
         if (isEnabled()) {
-            String[] tokens = apce.getMessage().split(" ");
-            if (tokens.length > 0 && "P".equals(tokens[0])) {
+            //if the message started with a P
+            if(apce.getMessage().startsWith("P ")){
+                //get the player name and cancel the event
                 String playerName = apce.getPlayer().getName();
                 apce.setCancelled(true);
-                Bukkit.getScheduler().scheduleSyncDelayedTask(UltraHardcore.getInstance(), new ChatRunnable(playerName, tokens));
+                //schedule the new message to be send on the next tick
+                Bukkit.getScheduler().scheduleSyncDelayedTask(UltraHardcore.getInstance(), new ChatRunnable(playerName, apce.getMessage()));
             }
         }
     }
@@ -41,38 +55,43 @@ public class AnonChat extends UHCFeature {
 
     private static class ChatRunnable implements Runnable {
 
-        public static final int CAPACITY = 256;
+        /**
+         * The player name sending the message
+         */
         private final String m_playerName;
-        private final String[] m_tokens;
+        /**
+         * The message they are sending
+         */
+        private final String m_message;
 
-        ChatRunnable(String playerName, String[] tokens) {
+        ChatRunnable(String playerName, String message) {
             m_playerName = playerName;
-            m_tokens = tokens;
+            m_message = message;
         }
 
         @Override
         public void run() {
-            Player p = Bukkit.getPlayer(m_playerName);
-            if (p == null) {
+            //attempt to get the player from the stored name if it exists
+            Player p = Bukkit.getPlayerExact(m_playerName);
+            if (null == p) {
                 return;
             }
+            //check the permissions
             if (!p.hasPermission(PermissionNodes.ANON_CHAT_CHAT)) {
                 p.sendMessage(ChatColor.RED + "You don't have the permissions to use annonymous chat");
                 return;
             }
-            StringBuilder buf = new StringBuilder(CAPACITY);
-            buf.append(PREFIX);
-            for (int i = 1; i < m_tokens.length; i++) {
-                buf.append(" ");
-                buf.append(m_tokens[i]);
-            }
-            String finalMessage = buf.toString();
+
+            //create the message and send it to all players online
+            String finalMessage = PREFIX+m_message;
             for (Player pl : Bukkit.getOnlinePlayers()) {
+                //check for the see name permission and send the actual name along with the message
                 if (pl.hasPermission(PermissionNodes.ANON_CHAT_SEE_NAME)) {
                     pl.sendMessage(String.valueOf(ChatColor.GRAY) + ChatColor.ITALIC + m_playerName + finalMessage);
                 } else {
                     pl.sendMessage(PREFIX + finalMessage);
                 }
+                //log the anonchat usage
                 Bukkit.getLogger().info("[AnonChat][" + m_playerName + "]" + finalMessage);
             }
         }
