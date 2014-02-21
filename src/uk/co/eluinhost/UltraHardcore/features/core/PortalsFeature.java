eb7d9b0d96b0ff12b49f73a816567ba24ff63664
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-21 11:15:52 +0000

    docs

--- src/uk/co/eluinhost/UltraHardcore/features/core/PortalsFeature.java
+++ src/uk/co/eluinhost/UltraHardcore/features/core/PortalsFeature.java
@@ -18,17 +18,27 @@ public class PortalsFeature extends UHCFeature {
 
     @EventHandler
     public void onPortalEvent(PlayerPortalEvent entityPortalEvent) {
+        //if we're enabled
         if (isEnabled()) {
-            FileConfiguration c = ConfigHandler.getConfig(ConfigHandler.MAIN);
+            //get the config
+            FileConfiguration config = ConfigHandler.getConfig(ConfigHandler.MAIN);
+
+            //create a travel agent for the portal
             TravelAgent ta = entityPortalEvent.getPortalTravelAgent();
+
+            //if they're in the nether
             if (entityPortalEvent.getPlayer().getWorld().getEnvironment() == World.Environment.NETHER) {
-                ta.setCanCreatePortal(c.getBoolean(ConfigNodes.PORTAL_RANGES_FROM_NETHER_ALLOWED));
-                ta.setCreationRadius(c.getInt(ConfigNodes.PORTAL_RANGES_FROM_NETHER_CREATION));
-                ta.setSearchRadius(c.getInt(ConfigNodes.PORTAL_RANGES_FROM_NETHER_SEARCH));
+
+                //set data from the nether
+                ta.setCanCreatePortal(config.getBoolean(ConfigNodes.PORTAL_RANGES_FROM_NETHER_ALLOWED));
+                ta.setCreationRadius(config.getInt(ConfigNodes.PORTAL_RANGES_FROM_NETHER_CREATION));
+                ta.setSearchRadius(config.getInt(ConfigNodes.PORTAL_RANGES_FROM_NETHER_SEARCH));
             } else {
-                ta.setCanCreatePortal(c.getBoolean(ConfigNodes.PORTAL_RANGES_TO_NETHER_ALLOWED));
-                ta.setCreationRadius(c.getInt(ConfigNodes.PORTAL_RANGES_TO_NETHER_CREATION));
-                ta.setSearchRadius(c.getInt(ConfigNodes.PORTAL_RANGES_TO_NETHER_SEARCH));
+
+                //set the data to the nether
+                ta.setCanCreatePortal(config.getBoolean(ConfigNodes.PORTAL_RANGES_TO_NETHER_ALLOWED));
+                ta.setCreationRadius(config.getInt(ConfigNodes.PORTAL_RANGES_TO_NETHER_CREATION));
+                ta.setSearchRadius(config.getInt(ConfigNodes.PORTAL_RANGES_TO_NETHER_SEARCH));
             }
         }
     }
