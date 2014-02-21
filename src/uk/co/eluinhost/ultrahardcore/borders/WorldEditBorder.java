e3908ce8c6810f1c8c5424a86425919149d79821
Graham <graham_howden1@yahoo.co.uk>
2014-02-21 22:42:48 +0000

    add hashCode

--- src/uk/co/eluinhost/ultrahardcore/borders/WorldEditBorder.java
+++ src/uk/co/eluinhost/ultrahardcore/borders/WorldEditBorder.java
@@ -2,6 +2,7 @@ package uk.co.eluinhost.ultrahardcore.borders;
 
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.MaxChangedBlocksException;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 public abstract class WorldEditBorder {
 
@@ -12,7 +13,12 @@ public abstract class WorldEditBorder {
     protected abstract void createBorder(BorderParams bp, EditSession es) throws MaxChangedBlocksException;
 
     @Override
-    public boolean equals(Object o) {
-        return o instanceof WorldEditBorder && ((WorldEditBorder) o).getID().equals(getID());
+    public boolean equals(Object obj) {
+        return obj instanceof WorldEditBorder && ((WorldEditBorder) obj).getID().equals(getID());
+    }
+
+    @Override
+    public int hashCode(){
+        return new HashCodeBuilder(17, 31).append(getID()).toHashCode();
     }
 }
