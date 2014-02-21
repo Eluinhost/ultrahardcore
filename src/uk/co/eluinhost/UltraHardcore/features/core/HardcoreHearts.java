8cfe05c1ab2b3c9bc372fb1bbbfda453ce688b1c
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-21 12:20:44 +0000

    docs,refactor

--- src/uk/co/eluinhost/UltraHardcore/features/core/HardcoreHearts.java
+++ src/uk/co/eluinhost/UltraHardcore/features/core/HardcoreHearts.java
@@ -3,16 +3,15 @@ package uk.co.eluinhost.UltraHardcore.features.core;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.*;
+import org.bukkit.plugin.Plugin;
 import uk.co.eluinhost.UltraHardcore.UltraHardcore;
 import uk.co.eluinhost.UltraHardcore.features.UHCFeature;
 
 public class HardcoreHearts extends UHCFeature {
 
-    private ProtocolManager pm = ProtocolLibrary.getProtocolManager();
-    private PacketAdapter pa = null;
+    private final ProtocolManager m_manager = ProtocolLibrary.getProtocolManager();
+    private final PacketListener m_packetAdapter = new HardcoreHeartsListener();
 
     public HardcoreHearts(boolean enabled) {
         super("HardcoreHearts", enabled);
@@ -21,27 +20,27 @@ public class HardcoreHearts extends UHCFeature {
 
     @Override
     public void enableFeature() {
-        if (pa == null) {
-            //pa = new PacketAdapter(UltraHardcore.getInstance(), ConnectionSide.SERVER_SIDE,
-            //        ListenerPriority.NORMAL, GamePhase.LOGIN, Packets.Server.LOGIN){
-            pa = new PacketAdapter(UltraHardcore.getInstance(), ListenerPriority.NORMAL,
-                    PacketType.Play.Server.LOGIN) {
-                @Override
-                public void onPacketSending(PacketEvent event) {
-                    if (event.getPacketType() == PacketType.Play.Server.LOGIN) {
-                        event.getPacket().getBooleans().write(0, true);
-                    }
-                }
-            };
-        }
-        pm.addPacketListener(pa);
+        m_manager.addPacketListener(m_packetAdapter);
     }
 
     @Override
     public void disableFeature() {
-        if (pa != null) {
-            pm.removePacketListener(pa);
-        }
+        m_manager.removePacketListener(m_packetAdapter);
     }
 
+    private static class HardcoreHeartsListener extends PacketAdapter {
+
+        HardcoreHeartsListener(){
+            //listen for login packets on the normal priority
+            super(UltraHardcore.getInstance(),ListenerPriority.NORMAL,PacketType.Play.Server.LOGIN);
+        }
+
+        @Override
+        public void onPacketSending(PacketEvent event) {
+            //if its a login packet write the first boolean to true (hardcore flag)
+            if (event.getPacketType().equals(PacketType.Play.Server.LOGIN)) {
+                event.getPacket().getBooleans().write(0, true);
+            }
+        }
+    }
 }
