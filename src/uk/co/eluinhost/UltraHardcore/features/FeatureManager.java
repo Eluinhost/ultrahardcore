f55f07f3536edfb37ff86d8a9d358d1d96892f4e
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-21 10:04:08 +0000

    reformatting

--- src/uk/co/eluinhost/UltraHardcore/features/FeatureManager.java
+++ src/uk/co/eluinhost/UltraHardcore/features/FeatureManager.java
@@ -18,34 +18,35 @@ public class FeatureManager {
     /**
      * Stores a list of all the features loaded on the server
      */
-	private static ArrayList<UHCFeature> features = new ArrayList<UHCFeature>();
+    private static ArrayList<UHCFeature> features = new ArrayList<UHCFeature>();
 
     /**
      * Only allow features with this pattern as an ID
      */
-	private final static Pattern name_pattern = Pattern.compile("^[\\w]++$");
+    private final static Pattern name_pattern = Pattern.compile("^[\\w]++$");
 
     /**
      * Add a UHC feature to the manager
+     *
      * @param feature UHCFeature the feature to be added
      * @param enabled Whether the feature should be enabled or not after init
      * @throws FeatureIDConflictException when feature with the same ID already exists
-     * @throws InvalidFeatureIDException when the feature has an invalid ID name
+     * @throws InvalidFeatureIDException  when the feature has an invalid ID name
      */
-	public static void addFeature(UHCFeature feature,boolean enabled) throws FeatureIDConflictException, InvalidFeatureIDException{
+    public static void addFeature(UHCFeature feature, boolean enabled) throws FeatureIDConflictException, InvalidFeatureIDException {
 
         //check for alphanumerics
-		Matcher mat = name_pattern.matcher(feature.getFeatureID());
-		if(!mat.matches()){
-			throw new InvalidFeatureIDException();
-		}
+        Matcher mat = name_pattern.matcher(feature.getFeatureID());
+        if (!mat.matches()) {
+            throw new InvalidFeatureIDException();
+        }
 
         //check for existing feature of the same name
-		for(UHCFeature f : features){
-			if(f.getFeatureID().equals(feature.getFeatureID())){
-				throw new FeatureIDConflictException();
-			}
-		}
+        for (UHCFeature f : features) {
+            if (f.getFeatureID().equals(feature.getFeatureID())) {
+                throw new FeatureIDConflictException();
+            }
+        }
 
         //Make an init event for the feature creation
         UHCFeatureInitEvent uhc_init_event = new UHCFeatureInitEvent(feature);
@@ -54,71 +55,75 @@ public class FeatureManager {
         Bukkit.getServer().getPluginManager().callEvent(uhc_init_event);
 
         //if it was cancelled return
-        if(!uhc_init_event.isAllowed()){
+        if (!uhc_init_event.isAllowed()) {
             return;
         }
 
         //add the feature
 
         //TODO change this >.>
-		features.add(feature);
-		if(feature.isEnabled()){
-			feature.enableFeature();
-		}else{
-			feature.disableFeature();
-		}
+        features.add(feature);
+        if (feature.isEnabled()) {
+            feature.enableFeature();
+        } else {
+            feature.disableFeature();
+        }
 
         //Register the feature for plugin events
-		Bukkit.getPluginManager().registerEvents(feature, UltraHardcore.getInstance());
-	}
+        Bukkit.getPluginManager().registerEvents(feature, UltraHardcore.getInstance());
+    }
 
     /**
      * Check if a feature is enabled by it's ID
+     *
      * @param ID String the ID to check for
      * @return boolean true if enabled, false otherwise
      * @throws FeatureIDNotFoundException when feature not found
      */
-	public static boolean isEnabled(String ID) throws FeatureIDNotFoundException{
-		for(UHCFeature feature : features){
-			if(feature.getFeatureID().equals(ID)){
-				return feature.isEnabled();
-			}
-		}
-		throw new FeatureIDNotFoundException();
-	}
+    public static boolean isEnabled(String ID) throws FeatureIDNotFoundException {
+        for (UHCFeature feature : features) {
+            if (feature.getFeatureID().equals(ID)) {
+                return feature.isEnabled();
+            }
+        }
+        throw new FeatureIDNotFoundException();
+    }
 
     /**
      * Get the UHCFeature based on it's ID
+     *
      * @param ID String the ID to check for
      * @return UHCFeature the returned feature
      * @throws FeatureIDNotFoundException when feature ID not found
      */
-	public static UHCFeature getFeature(String ID) throws FeatureIDNotFoundException{
-		for(UHCFeature feature : features){
-			if(feature.getFeatureID().equals(ID)){
-				return feature;
-			}
-		}
-		throw new FeatureIDNotFoundException();
-	}
+    public static UHCFeature getFeature(String ID) throws FeatureIDNotFoundException {
+        for (UHCFeature feature : features) {
+            if (feature.getFeatureID().equals(ID)) {
+                return feature;
+            }
+        }
+        throw new FeatureIDNotFoundException();
+    }
 
     /**
      * Returns all of the features loaded
+     *
      * @return ArrayList
      */
-	public static ArrayList<UHCFeature> getFeatures(){
-		return features;
-	}
+    public static ArrayList<UHCFeature> getFeatures() {
+        return features;
+    }
 
     /**
      * Get a list of all the used feature names
+     *
      * @return List String
      */
-	public static List<String> getFeatureNames(){
-		ArrayList<String> f = new ArrayList<String>();
-		for(UHCFeature uhc : features){
-			f.add(uhc.getFeatureID());
-		}
-		return f;
-	}
+    public static List<String> getFeatureNames() {
+        ArrayList<String> f = new ArrayList<String>();
+        for (UHCFeature uhc : features) {
+            f.add(uhc.getFeatureID());
+        }
+        return f;
+    }
 }
--- src/uk/co/eluinhost/UltraHardcore/features/UHCFeature.java
+++ src/uk/co/eluinhost/UltraHardcore/features/UHCFeature.java
@@ -4,54 +4,57 @@ import org.bukkit.event.Listener;
 
 import uk.co.eluinhost.UltraHardcore.exceptions.FeatureStateNotChangedException;
 
-public abstract class UHCFeature implements Listener{
+public abstract class UHCFeature implements Listener {
 
     /**
      * The feature ID for the feature, override to set one for your feature, allows [\w]++
      */
-	private String featureID = null;
-	private boolean enabled = false;
-	private String description = "N/A";
-		
-	public abstract void enableFeature();
-	public abstract void disableFeature();
+    private String featureID = null;
+    private boolean enabled = false;
+    private String description = "N/A";
 
-	public final String getFeatureID() {
-		return featureID;
-	}
+    public abstract void enableFeature();
 
-	public final boolean isEnabled() {
-		return enabled;
-	}
-	
-	public final void setEnabled(boolean enable) throws FeatureStateNotChangedException {
-		//if we're changing state
-		if(enable != isEnabled()){
-			if(enable){
-				enabled = true;
-				enableFeature();
-			}else{
-				enabled = false;
-				disableFeature();
-			}
-		}else{
-			throw new FeatureStateNotChangedException();
-		}
-	}
+    public abstract void disableFeature();
 
-    public UHCFeature(String featureID,boolean enabled){
-		this.featureID = featureID;
+    public final String getFeatureID() {
+        return featureID;
+    }
+
+    public final boolean isEnabled() {
+        return enabled;
+    }
+
+    public final void setEnabled(boolean enable) throws FeatureStateNotChangedException {
+        //if we're changing state
+        if (enable != isEnabled()) {
+            if (enable) {
+                enabled = true;
+                enableFeature();
+            } else {
+                enabled = false;
+                disableFeature();
+            }
+        } else {
+            throw new FeatureStateNotChangedException();
+        }
+    }
+
+    public UHCFeature(String featureID, boolean enabled) {
+        this.featureID = featureID;
         this.enabled = enabled;
-	}
-	
-	@Override
-	public boolean equals(Object o){
-		return (o instanceof UHCFeature && ((UHCFeature)o).getFeatureID().equals(this.getFeatureID()));
-	}
-	public final String getDescription() {
-		return description;
-	}
-	public final void setDescription(String description) {
-		this.description = description;
-	}
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof UHCFeature && ((UHCFeature) o).getFeatureID().equals(this.getFeatureID()));
+    }
+
+    public final String getDescription() {
+        return description;
+    }
+
+    public final void setDescription(String description) {
+        this.description = description;
+    }
 }
