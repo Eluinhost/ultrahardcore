8dc31c7507e118bf7daa2c125599cb1eebf5cd20
Graham <graham_howden1@yahoo.co.uk>
2014-02-22 11:46:32 +0000

    docs

--- src/uk/co/eluinhost/ultrahardcore/scatter/PlayerTeleportMapping.java
+++ src/uk/co/eluinhost/ultrahardcore/scatter/PlayerTeleportMapping.java
@@ -2,10 +2,11 @@ package uk.co.eluinhost.ultrahardcore.scatter;
 
 import org.bukkit.Location;
 
+//TODO add teleport logic into here?
 public class PlayerTeleportMapping {
 
     private final String m_playerName;
-    private int m_amountTried = 0;
+    private int m_amountTried;
     private final Location m_location;
     private final String m_team;
 
@@ -15,22 +16,41 @@ public class PlayerTeleportMapping {
         m_team = teamName;
     }
 
+    /**
+     * Get the amount of times this mapping has been attempted
+     * @return int
+     */
     public int getAmountTried() {
         return m_amountTried;
     }
 
+    /**
+     * Increase the amount of times tried
+     */
     public void incrementAmountTried() {
         m_amountTried++;
     }
 
+    /**
+     * Get the name of the player to teleport
+     * @return String
+     */
     public String getPlayerName() {
         return m_playerName;
     }
 
+    /**
+     * Get the location to teleport to
+     * @return Location
+     */
     public Location getLocation() {
         return m_location;
     }
 
+    /**
+     * Get the name of the team to teleport as
+     * @return String
+     */
     public String getTeamName() {
         return m_team;
     }
