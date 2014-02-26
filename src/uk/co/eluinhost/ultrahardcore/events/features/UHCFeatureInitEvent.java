140978f67ca616c4604b66520c680bd1814ae5e2
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-26 08:58:00 +0000

    add to javadoc

--- src/uk/co/eluinhost/ultrahardcore/events/features/UHCFeatureInitEvent.java
+++ src/uk/co/eluinhost/ultrahardcore/events/features/UHCFeatureInitEvent.java
@@ -4,7 +4,7 @@ import uk.co.eluinhost.ultrahardcore.features.IUHCFeature;
 
 public class UHCFeatureInitEvent extends UHCFeatureEvent{
     /**
-     * Called when a feature is first initialized, cancelling stops the feature from loading
+     * Called when a feature is first initialized, cancelling stops the feature from getting events and being added to the list
      * @param feature the feature involved
      */
     public UHCFeatureInitEvent(IUHCFeature feature) {
