d5c93eaeb6c991fc73c6608dfd7b8e2da022424d
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-27 09:55:09 +0000

    fix to new params

--- src/uk/co/eluinhost/ultrahardcore/borders/types/SquareBorder.java
+++ src/uk/co/eluinhost/ultrahardcore/borders/types/SquareBorder.java
@@ -6,28 +6,32 @@ import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.blocks.BaseBlock;
 import com.sk89q.worldedit.patterns.SingleBlockPattern;
 import com.sk89q.worldedit.regions.CuboidRegion;
+import org.bukkit.Location;
 import uk.co.eluinhost.ultrahardcore.borders.BorderParams;
 
 public class SquareBorder extends WorldEditBorder {
 
     public static final int MAX_HEIGHT = 256;
 
+    /**
+     * Make a square border
+     */
     public SquareBorder() {
         super("Square", "Creates a square wall around the map");
     }
 
     @Override
     public void createBorder(BorderParams bp, EditSession es) throws MaxChangedBlocksException {
+        Location center = bp.getCenter();
+        int radius = bp.getRadius();
         Vector pos1 = new Vector(
-                bp.getX() + bp.getRadius(),
+                center.getBlockX() + radius,
                 MAX_HEIGHT,
-                bp.getZ() + bp.getRadius());
+                center.getBlockZ() + radius);
         Vector pos2 = new Vector(
-                bp.getX() - bp.getRadius(),
+                center.getBlockX() - radius,
                 0,
-                bp.getZ() - bp.getRadius());
-        es.makeCuboidWalls(new CuboidRegion(pos1, pos2)
-                , new SingleBlockPattern(
-                new BaseBlock(bp.getBlockID(), bp.getBlockMeta())));
+                center.getBlockZ() - radius);
+        es.makeCuboidWalls(new CuboidRegion(pos1, pos2), new SingleBlockPattern(new BaseBlock(bp.getBlockID(), bp.getBlockMeta())));
     }
 }
