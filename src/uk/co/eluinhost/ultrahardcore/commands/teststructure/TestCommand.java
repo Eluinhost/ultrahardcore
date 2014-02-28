5ce3a3c33342dd473cdc27e44c25619847a80eb3
HowdenG <graham_howden1@yahoo.co.uk>
2014-02-28 16:13:00 +0000

    update examples

--- src/uk/co/eluinhost/ultrahardcore/commands/teststructure/TestCommand.java
+++ src/uk/co/eluinhost/ultrahardcore/commands/teststructure/TestCommand.java
@@ -2,18 +2,24 @@ package uk.co.eluinhost.ultrahardcore.commands.teststructure;
 
 public class TestCommand {
 
-    @Command(name="test", id="test-root", allowedTypes = {SenderType.CONSOLE, SenderType.REMOTE_CONSOLE})
-    public void testCommandMethod(CommandRequest request){
-        //TODO
-    }
+    /**
+     * runs with '/test'
+     * @param request
+     */
+    @Command(trigger="test", identifier="TestCommand-root")
+    public void testCommandMethod(CommandRequest request){}
 
-    @Command(name="subtest1", id="test-sub-1", parentID = "test-root", allowedTypes = {SenderType.CONSOLE, SenderType.REMOTE_CONSOLE})
-    public void testCommandMethod2(CommandRequest request){
-        //TODO
-    }
+    /**
+     * runs with '/test sub-1'
+     * @param request
+     */
+    @Command(trigger="sub-1", identifier="TestCommand-sub-1", parentID = "TestCommand-root")
+    public void testCommandMethod2(CommandRequest request){}
 
-    @Command(name="subtest2", id="test-sub-2", parentID = "test-root", allowedTypes = {SenderType.CONSOLE, SenderType.REMOTE_CONSOLE})
-    public void testCommandMethod3(CommandRequest request){
-        //TODO
-    }
+    /**
+     * runs with '/test sub-2'
+     * @param request
+     */
+    @Command(trigger="sub-2", identifier="TestCommand-sub-2", parentID = "TestCommand-root")
+    public void testCommandMethod3(CommandRequest request){}
 }
