4d80ea610eedba3e1d782c9947e0a58cb6c7f641
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-06 14:57:51 +0000

    add permission to use nether portals when feature enabled

--- src/uk/co/eluinhost/ultrahardcore/features/nether/NetherFeature.java
+++ src/uk/co/eluinhost/ultrahardcore/features/nether/NetherFeature.java
@@ -1,8 +1,10 @@
 package uk.co.eluinhost.ultrahardcore.features.nether;
 
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.permissions.Permissible;
 import uk.co.eluinhost.ultrahardcore.features.UHCFeature;
 
 
@@ -12,6 +14,8 @@ import uk.co.eluinhost.ultrahardcore.features.UHCFeature;
  * @author ghowden
  */
 public class NetherFeature extends UHCFeature {
+
+    public static final String ALLOW_NETHER = BASE_PERMISSION+"nether.allow";
 
     /**
      * Stops travelling to the nether when enabled
@@ -27,9 +31,11 @@ public class NetherFeature extends UHCFeature {
     @EventHandler
     public void onPortalEvent(EntityPortalEvent epe) {
         //if it's enabled
-        if (isEnabled()) {
+        if (isEnabled() && epe.getEntity() instanceof Permissible) {
             //if they're going into the nether cancel it
-            //TODO add a permission for this
+            if(((Permissible) epe.getEntity()).hasPermission(ALLOW_NETHER)){
+                return;
+            }
             if (epe.getTo().getWorld().getEnvironment() == World.Environment.NETHER) {
                 epe.setCancelled(true);
             }
