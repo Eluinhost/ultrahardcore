0c9d8d842fe81e8bdee990f4764f020907592f53
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-06 15:21:29 +0000

    all but list of players done

--- src/uk/co/eluinhost/ultrahardcore/commands/scatter/ScatterCommand.java
+++ src/uk/co/eluinhost/ultrahardcore/commands/scatter/ScatterCommand.java
@@ -1,14 +1,22 @@
 package uk.co.eluinhost.ultrahardcore.commands.scatter;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.conversations.Conversable;
 import org.bukkit.conversations.ConversationFactory;
+import org.bukkit.entity.Player;
 import uk.co.eluinhost.commands.Command;
 import uk.co.eluinhost.commands.CommandRequest;
 import uk.co.eluinhost.commands.SenderType;
 import uk.co.eluinhost.ultrahardcore.UltraHardcore;
+import uk.co.eluinhost.ultrahardcore.scatter.Parameters;
 import uk.co.eluinhost.ultrahardcore.scatter.ScatterManager;
+import uk.co.eluinhost.ultrahardcore.scatter.exceptions.MaxAttemptsReachedException;
+import uk.co.eluinhost.ultrahardcore.scatter.types.AbstractScatterType;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ScatterCommand {
@@ -48,7 +56,68 @@ public class ScatterCommand {
             minArgs = 7,
             permission = SCATTER_COMMAND)
     public void onScatterCommand(CommandRequest request){
-        //TODO process and do things
+        ScatterManager manager = ScatterManager.getInstance();
+        if(manager.isScatterInProgress()){
+            request.sendMessage(ChatColor.RED+"There is already a scatter in progress, please wait and try again");
+            return;
+        }
+
+        AbstractScatterType scatterType = manager.getScatterType(request.getArg(0));
+        if(scatterType == null){
+            request.sendMessage(ChatColor.RED+"Invalid scatter type, use /scatter types to see a list of types");
+            return;
+        }
+        World world = request.getWorld(1);
+        if(world == null){
+            request.sendMessage(ChatColor.RED+"Invalid world supplied");
+            return;
+        }
+        String coordString = request.getArg(2);
+        String[] parts = coordString.split(",");
+        if(parts.length != 2){
+            request.sendMessage(ChatColor.RED+"Invalid coordinates supplied");
+            return;
+        }
+        double x,z;
+        try{
+            x = Double.valueOf(parts[0]);
+            z = Double.valueOf(parts[1]);
+        }catch(NumberFormatException ignored){
+            request.sendMessage(ChatColor.RED+"Invalid coordinates supplied");
+            return;
+        }
+        if(!request.isArgNumber(3)){
+            request.sendMessage(ChatColor.RED+"Invalid radius supplied");
+            return;
+        }
+        double radius = request.getNumber(3).doubleValue();
+        if(!request.isArgNumber(4)){
+            request.sendMessage(ChatColor.RED+"Invalid min distance supplied");
+            return;
+        }
+        double minDistance = request.getNumber(4).doubleValue();
+        if(!request.isArgBoolean(5)){
+            request.sendMessage(ChatColor.RED+"Invalid value supplied for use teams true/false");
+            return;
+        }
+        boolean asTeam = request.getBoolean(5);
+        Iterable<Player> scatterPlayers = new LinkedList<Player>();
+        //TODO parse list of players
+
+        Parameters params = new Parameters(new Location(world,x,0,z));
+        params.setRadius(radius);
+        params.setMinimumDistance(minDistance);
+        params.setAsTeam(asTeam);
+
+        //TODO add allowed blocks somewhere
+
+        Conversable conversable = request.getSender() instanceof Conversable ? (Conversable) request.getSender() : Bukkit.getConsoleSender();
+
+        try {
+            manager.scatter(scatterType,params,scatterPlayers, conversable);
+        } catch (MaxAttemptsReachedException ignored) {
+            request.sendMessage(ChatColor.RED+"Hit max attempts at finding enough scatter locations, please try another scatter type and/or parameters");
+        }
     }
 
     /**
