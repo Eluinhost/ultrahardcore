fdf00a6f518e45d9429da92ecadc00f0aa1be62e
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-06 14:44:44 +0000

    allow scatter types

--- src/uk/co/eluinhost/ultrahardcore/commands/scatter/ScatterTypePrompt.java
+++ src/uk/co/eluinhost/ultrahardcore/commands/scatter/ScatterTypePrompt.java
@@ -3,22 +3,28 @@ package uk.co.eluinhost.ultrahardcore.commands.scatter;
 import org.bukkit.conversations.ConversationContext;
 import org.bukkit.conversations.FixedSetPrompt;
 import org.bukkit.conversations.Prompt;
+import uk.co.eluinhost.ultrahardcore.scatter.ScatterManager;
 
 public class ScatterTypePrompt extends FixedSetPrompt {
 
     public static final String TYPE_DATA = "scatter_type";
 
+    /**
+     * Allows any of the loaded scatter type names
+     */
     public ScatterTypePrompt(){
-        super(/*TODO types*/);
+        //noinspection AssignmentToSuperclassField
+        fixedSet = ScatterManager.getInstance().getScatterTypeNames();
     }
 
     @Override
     protected Prompt acceptValidatedInput(ConversationContext conversationContext, String s) {
-        return null;
+        conversationContext.setSessionData(TYPE_DATA,ScatterManager.getInstance().getScatterType(s));
+        return new ScatterWorldPrompt();
     }
 
     @Override
     public String getPromptText(ConversationContext conversationContext) {
-        return null;
+        return "Enter type of scatter to use: "+formatFixedSet();
     }
 }
