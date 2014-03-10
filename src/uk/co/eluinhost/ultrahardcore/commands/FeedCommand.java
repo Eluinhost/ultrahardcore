b29f1ddb52b6485b68794ab9e1169f90278d2c35
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-10 09:49:55 +0000

    unused import

--- src/uk/co/eluinhost/ultrahardcore/commands/FeedCommand.java
+++ src/uk/co/eluinhost/ultrahardcore/commands/FeedCommand.java
@@ -1,7 +1,6 @@
 package uk.co.eluinhost.ultrahardcore.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import uk.co.eluinhost.commands.Command;
