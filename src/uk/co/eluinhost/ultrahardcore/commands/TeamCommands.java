b0f9dd026dea82d664b22963043e94ef2d4e6a90
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-10 12:43:01 +0000

    unused import

--- src/uk/co/eluinhost/ultrahardcore/commands/TeamCommands.java
+++ src/uk/co/eluinhost/ultrahardcore/commands/TeamCommands.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
