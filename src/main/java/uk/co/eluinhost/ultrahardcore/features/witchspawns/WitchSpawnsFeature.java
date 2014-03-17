070d0e9f9c45c392b49bdf14997ece16cdc2a6ef
HowdenG <graham_howden1@yahoo.co.uk>
2014-03-17 17:02:51 +0000

    refix witch spawn feature

--- src/main/java/uk/co/eluinhost/ultrahardcore/features/witchspawns/WitchSpawnsFeature.java
+++ src/main/java/uk/co/eluinhost/ultrahardcore/features/witchspawns/WitchSpawnsFeature.java
@@ -1,5 +1,7 @@
 package uk.co.eluinhost.ultrahardcore.features.witchspawns;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -7,13 +9,17 @@ import org.bukkit.plugin.Plugin;
 import uk.co.eluinhost.configuration.ConfigManager;
 import uk.co.eluinhost.ultrahardcore.features.UHCFeature;
 
+@Singleton
 public class WitchSpawnsFeature extends UHCFeature {
 
     /**
      * Feature allows witches to spawn when enabled, disables it when disabled
+     * @param plugin the plugin
+     * @param configManager the config manager
      */
+    @Inject
     public WitchSpawnsFeature(Plugin plugin, ConfigManager configManager) {
-        super(plugin, "WitchSpawns","Allows natural witch spawns", configManager);
+        super(plugin, configManager);
     }
 
     /**
@@ -26,5 +32,15 @@ public class WitchSpawnsFeature extends UHCFeature {
         if (!isEnabled() && ese.getSpawnReason() == CreatureSpawnEvent.SpawnReason.NATURAL && ese.getEntity().getType() == EntityType.WITCH) {
             ese.setCancelled(true);
         }
+    }
+
+    @Override
+    public String getFeatureID() {
+        return "WitchSpawns";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Allows natural witch spawns";
     }
 }
