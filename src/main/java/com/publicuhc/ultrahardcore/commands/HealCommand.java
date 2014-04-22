af00333c675a7840b64c93c1603353ce52e8f115
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-22 12:10:12 +0100

    convert heal commands

--- src/main/java/com/publicuhc/ultrahardcore/commands/HealCommand.java
+++ src/main/java/com/publicuhc/ultrahardcore/commands/HealCommand.java
@@ -1,8 +1,13 @@
 package com.publicuhc.ultrahardcore.commands;
 
-import com.publicuhc.commands.Command;
-import com.publicuhc.commands.CommandRequest;
-import com.publicuhc.commands.SenderType;
+import com.publicuhc.pluginframework.commands.annotation.CommandMethod;
+import com.publicuhc.pluginframework.commands.annotation.RouteInfo;
+import com.publicuhc.pluginframework.commands.matchers.AnyRouteMatcher;
+import com.publicuhc.pluginframework.commands.matchers.PatternRouteMatcher;
+import com.publicuhc.pluginframework.commands.requests.CommandRequest;
+import com.publicuhc.pluginframework.commands.requests.SenderType;
+import com.publicuhc.pluginframework.commands.routing.DefaultMethodRoute;
+import com.publicuhc.pluginframework.commands.routing.MethodRoute;
 import com.publicuhc.pluginframework.configuration.Configurator;
 import com.publicuhc.pluginframework.shaded.inject.Inject;
 import com.publicuhc.pluginframework.translate.Translate;
@@ -12,6 +17,7 @@ import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class HealCommand extends SimpleCommand {
 
@@ -25,16 +31,11 @@ public class HealCommand extends SimpleCommand {
     }
 
     /**
-     * Ran on /healself
+     * heal yourself
      * @param request request params
      */
-    @Command(trigger = "healself",
-            identifier = "HealSelfCommand",
-            minArgs = 0,
-            maxArgs = 0,
-            senders = {SenderType.PLAYER},
-            permission = HEAL_SELF_PERMISSION)
-    public void onHealSelfCommand(CommandRequest request){
+    @CommandMethod
+    public void healSelfCommand(CommandRequest request){
         Player player = (Player) request.getSender();
         player.setHealth(player.getMaxHealth());
         player.sendMessage(translate("heal.tell", locale(request.getSender())));
@@ -45,15 +46,27 @@ public class HealCommand extends SimpleCommand {
     }
 
     /**
-     * Ran on /heal {name}
+     * Run on /healself.*
+     * @return the route
+     */
+    @RouteInfo
+    public MethodRoute healSelfCommandDetails() {
+        return new DefaultMethodRoute(
+                new AnyRouteMatcher(),
+                new SenderType[] {
+                        SenderType.PLAYER
+                },
+                HEAL_SELF_PERMISSION,
+                "healself"
+        );
+    }
+
+    /**
+     * heal someone else
      * @param request request params
      */
-    @Command(trigger = "heal",
-            identifier = "HealCommand",
-            minArgs = 1,
-            maxArgs = 1,
-            permission = HEAL_OTHER_PERMISSION)
-    public void onHealCommand(CommandRequest request){
+    @CommandMethod
+    public void healCommand(CommandRequest request){
         Player p = Bukkit.getPlayer(request.getFirstArg());
         if (p == null) {
             request.sendMessage(translate("heal.invalid_player", locale(request.getSender()), "name", request.getFirstArg()));
@@ -69,21 +82,54 @@ public class HealCommand extends SimpleCommand {
     }
 
     /**
+     * Run on /heal .+ except '/heal *'
+     * @return the route
+     */
+    @RouteInfo
+    public MethodRoute healCommandDetails() {
+        return new DefaultMethodRoute(
+                new PatternRouteMatcher(Pattern.compile("[^*]+")),
+                new SenderType[] {
+                        SenderType.PLAYER,
+                        SenderType.CONSOLE,
+                        SenderType.COMMAND_BLOCK,
+                        SenderType.REMOTE_CONSOLE
+                },
+                HEAL_OTHER_PERMISSION,
+                "heal"
+        );
+    }
+
+    /**
      * Ran on /heal *
      * @param request request params
      */
-    @Command(trigger = "*",
-            identifier = "HealAllCommand",
-            parentID = "HealCommand",
-            permission = HEAL_OTHER_PERMISSION,
-            minArgs = 0,
-            maxArgs = 0)
-    public void onHealAllCommand(CommandRequest request){
+    @CommandMethod
+    public void healAllCommand(CommandRequest request){
         for (Player p : Bukkit.getOnlinePlayers()) {
             p.setHealth(p.getMaxHealth());
             p.sendMessage(translate("heal.tell", locale(request.getSender())));
         }
         request.sendMessage(translate("heal.heal_all", locale(request.getSender())));
         ServerUtil.broadcastForPermission(translate("heal.heal_all_announce", locale(request.getSender()), "name", request.getSender().getName()), HEAL_ANNOUNCE_PERMISSION);
+    }
+
+    /**
+     * Run on /heal * only
+     * @return the route
+     */
+    @RouteInfo
+    public MethodRoute healAllCommandDetails() {
+        return new DefaultMethodRoute(
+                new PatternRouteMatcher(Pattern.compile("\\*")),
+                new SenderType[] {
+                        SenderType.PLAYER,
+                        SenderType.CONSOLE,
+                        SenderType.COMMAND_BLOCK,
+                        SenderType.REMOTE_CONSOLE
+                },
+                HEAL_OTHER_PERMISSION,
+                "heal"
+        );
     }
 }
