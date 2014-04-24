a478cb59c3d7c1b61acf90f9bb5cc6c9bd5a7783
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-24 08:53:05 +0100

    include a test to avoid fail build

---
+++ src/test/java/com/publicuhc/ultrahardcore/UltraHardcoreTest.java
@@ -0,0 +1,12 @@
+package com.publicuhc.ultrahardcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class UltraHardcoreTest {
+
+    @Test
+    public void testCaseToAvoidFailBuild() {}
+}
