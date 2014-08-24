5a071eac236946c478113890951a26098092ae5e
Graham <graham_howden1@yahoo.co.uk>
2014-08-24 14:53:49 +0100

    update the regen feature

--- src/main/java/com/publicuhc/ultrahardcore/pluginfeatures/regen/RegenFeature.java
+++ src/main/java/com/publicuhc/ultrahardcore/pluginfeatures/regen/RegenFeature.java
@@ -21,23 +21,19 @@
 
 package com.publicuhc.ultrahardcore.pluginfeatures.regen;
 
-import com.publicuhc.pluginframework.configuration.Configurator;
-import com.publicuhc.pluginframework.shaded.inject.Inject;
 import com.publicuhc.pluginframework.shaded.inject.Singleton;
-import com.publicuhc.pluginframework.translate.Translate;
-import com.publicuhc.ultrahardcore.pluginfeatures.UHCFeature;
+import com.publicuhc.ultrahardcore.features.UHCFeature;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.plugin.Plugin;
 
 
 /**
- * RegenHandler
- * Handles the regeneration of players and cancels if its from being near full hunger
+ * RegenFeature
  *
- * @author ghowden
+ * Enabled: Disables health regen for players
+ * Disabled: Nothing
  */
 @Singleton
 public class RegenFeature extends UHCFeature {
@@ -46,41 +42,33 @@ public class RegenFeature extends UHCFeature {
     private static final double PLAYER_DEAD_HEALTH = 0.0;
     private static final float EXHAUSTION_OFFSET = 3.0F;
 
-    public static final String NO_HEALTH_REGEN = BASE_PERMISSION+ "disableRegen";
+    public static final String NO_HEALTH_REGEN = "UHC.disableRegen";
 
-    /**
-     * Disables natural regen when enabled, normal when disabled
-     * @param plugin the plugin
-     * @param configManager the config manager
-     * @param translate the translator
-     */
-    @Inject
-    private RegenFeature(Plugin plugin, Configurator configManager, Translate translate) {
-        super(plugin, configManager, translate);
-    }
-
-    /**
-     * Whenever an entity regains health
-     * @param erhe the entityregainhealthevent
-     */
     @EventHandler
     public void onHealthRegen(EntityRegainHealthEvent erhe) {
-        if (isEnabled()) {
-            //If its a player regen
-            if (erhe.getEntityType() == EntityType.PLAYER) {
-                //If the player is in a hardcore world
-                //If its just standard health regen
-                if (erhe.getRegainReason() == EntityRegainHealthEvent.RegainReason.SATIATED) {
-                    Player p = (Player) erhe.getEntity();
-                    if (p.hasPermission(NO_HEALTH_REGEN)) {
-                        if (p.getFoodLevel() >= FOOD_LEVEL && p.getHealth() > PLAYER_DEAD_HEALTH && p.getHealth() < p.getMaxHealth()) {
-                            p.setExhaustion(p.getExhaustion() - EXHAUSTION_OFFSET);
-                        }
-                        //Cancel the event to stop the regen
-                        erhe.setCancelled(true);
-                    }
-                }
+        if (!isEnabled())
+            return;
+
+        //If its a player regen
+        if (erhe.getEntityType() != EntityType.PLAYER)
+            return;
+
+
+        //If its just standard health regen
+        if (erhe.getRegainReason() != EntityRegainHealthEvent.RegainReason.SATIATED)
+            return;
+
+        Player p = (Player) erhe.getEntity();
+
+        if (p.hasPermission(NO_HEALTH_REGEN)) {
+
+            //this is a special addition due to a bukkit bug where it adds exhuastion even if the healing is cancelled
+            if (p.getFoodLevel() >= FOOD_LEVEL && p.getHealth() > PLAYER_DEAD_HEALTH && p.getHealth() < p.getMaxHealth()) {
+                p.setExhaustion(p.getExhaustion() - EXHAUSTION_OFFSET);
             }
+
+            //Cancel the event to stop the regen
+            erhe.setCancelled(true);
         }
     }
 
