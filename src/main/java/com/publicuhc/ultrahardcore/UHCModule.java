44eb46d3c8d73d16a77cc9510fc7281960fd914e
Graham <graham_howden1@yahoo.co.uk>
2014-08-24 11:28:05 +0100

    remove old stuff from the core module

--- src/main/java/com/publicuhc/ultrahardcore/UHCModule.java
+++ src/main/java/com/publicuhc/ultrahardcore/UHCModule.java
@@ -20,24 +20,14 @@
  */
 package com.publicuhc.ultrahardcore;
 
-import com.publicuhc.ultrahardcore.features.FeatureManager;
-import com.publicuhc.ultrahardcore.features.RealFeatureManager;
 import com.publicuhc.pluginframework.shaded.inject.AbstractModule;
-import com.publicuhc.ultrahardcore.borders.BorderTypeManager;
-import com.publicuhc.ultrahardcore.borders.RealBorderTypeManager;
-import com.publicuhc.ultrahardcore.scatter.FallProtector;
-import com.publicuhc.ultrahardcore.scatter.Protector;
-import com.publicuhc.ultrahardcore.scatter.RealScatterManager;
-import com.publicuhc.ultrahardcore.scatter.ScatterManager;
+import com.publicuhc.ultrahardcore.features.DefaultFeatureManager;
+import com.publicuhc.ultrahardcore.features.FeatureManager;
 
-@SuppressWarnings("OverlyCoupledMethod")
 public class UHCModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(FeatureManager.class).to(RealFeatureManager.class);
-        bind(BorderTypeManager.class).to(RealBorderTypeManager.class);
-        bind(Protector.class).to(FallProtector.class);
-        bind(ScatterManager.class).to(RealScatterManager.class);
+        bind(FeatureManager.class).to(DefaultFeatureManager.class);
     }
 }
