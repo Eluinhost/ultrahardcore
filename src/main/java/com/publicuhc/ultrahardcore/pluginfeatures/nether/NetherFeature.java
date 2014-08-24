c9927198f66c47e7989e1ee69e816bfea41adf54
Graham <graham_howden1@yahoo.co.uk>
2014-08-24 11:50:19 +0100

    update the nether feature

--- src/main/java/com/publicuhc/ultrahardcore/pluginfeatures/nether/NetherFeature.java
+++ src/main/java/com/publicuhc/ultrahardcore/pluginfeatures/nether/NetherFeature.java
@@ -21,53 +21,33 @@
 
 package com.publicuhc.ultrahardcore.pluginfeatures.nether;
 
-import com.publicuhc.pluginframework.configuration.Configurator;
-import com.publicuhc.pluginframework.shaded.inject.Inject;
 import com.publicuhc.pluginframework.shaded.inject.Singleton;
-import com.publicuhc.pluginframework.translate.Translate;
-import com.publicuhc.ultrahardcore.pluginfeatures.UHCFeature;
+import com.publicuhc.ultrahardcore.features.UHCFeature;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.permissions.Permissible;
-import org.bukkit.plugin.Plugin;
-
 
 /**
  * NetherFeature
  *
- * @author ghowden
+ * Enabled: Disables travelling into the nether
+ * Disabled: Nothing
  */
 @Singleton
 public class NetherFeature extends UHCFeature {
 
-    public static final String ALLOW_NETHER = BASE_PERMISSION+"nether.allow";
+    public static final String ALLOW_NETHER = "UHC.nether.allow";
 
-    /**
-     * Stops travelling to the nether when enabled
-     * @param plugin the plugin
-     * @param configManager the config manager
-     * @param translate the translator
-     */
-    @Inject
-    private NetherFeature(Plugin plugin, Configurator configManager, Translate translate) {
-        super(plugin, configManager, translate);
-    }
-
-    /**
-     * On portal events
-     * @param epe related event
-     */
     @EventHandler
     public void onPortalEvent(EntityPortalEvent epe) {
         //if it's enabled
         if (isEnabled() && epe.getEntity() instanceof Permissible) {
             //if they're going into the nether cancel it
-            if(((Permissible) epe.getEntity()).hasPermission(ALLOW_NETHER)){
-                return;
-            }
-            if (epe.getTo().getWorld().getEnvironment() == World.Environment.NETHER) {
-                epe.setCancelled(true);
+            if(!((Permissible) epe.getEntity()).hasPermission(ALLOW_NETHER)) {
+                if (epe.getTo().getWorld().getEnvironment() == World.Environment.NETHER) {
+                    epe.setCancelled(true);
+                }
             }
         }
     }
